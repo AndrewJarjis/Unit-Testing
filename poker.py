47e996b3bdb52a441a496db9211b59da59fcd940
andrewjarjis <andrewjarjis56@gmail.com>
2023-04-03 16:56:17 -0400

    Poker Testing

--- poker.py
+++ poker.py
@@ -1,10 +1,11 @@
-cards = ('S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK', 'SA')
+cards = {'S2': 2, 'S3': 3, 'S4': 4, 'S5': 5, 'S6': 6, 'S7': 7, 'S8': 8, 'S9': 9, 'S10': 10, 'SJ': 11, 'SQ': 12,
+         'SK': 13, 'SA': 14}
 
 
 def check_straight(card1, card2, card3):
-    cards = [card1, card2, card3]
+    current_cards = [card1, card2, card3]
     values = []
-    for card in cards:
+    for card in current_cards:
         if card[1:] == 'T':
             values.append(10)
         elif card[1:] == 'J':
@@ -23,15 +24,20 @@ def check_straight(card1, card2, card3):
     else:
         return 0
 
-
 def check_3ofa_kind(card1, card2, card3):
-    face_cards = {'A': 14, 'K': 13, 'Q': 12, 'J': 11}
-    cards = [card1, card2, card3]
-    values = [face_cards.get(card[1:], int(card[1:])) for card in cards]
+    current_cards = [card1, card2, card3]
+    face_values = {'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    values = []
+    for card in current_cards:
+        if card[1:] in face_values:
+            values.append(face_values[card[1:]])
+        else:
+            values.append(int(card[1:]))
     if len(set(values)) == 1:
         return values[0]
     else:
-        return 0
+        return 14
+
 
 
 def check_royal_flush(card1, card2, card3):
@@ -47,19 +53,19 @@ def play_cards(left1, left2, left3, right1, right2, right3):
     left_rank = get_rank(left_cards)
     right_rank = get_rank(right_cards)
     if left_rank > right_rank:
-        return -1
-    elif left_rank < right_rank:
         return 1
+    elif left_rank < right_rank:
+        return -1
     else:
         return 0
 
 
-def get_rank(cards):
-    cards.sort()
-    values = [int(card[1:]) for card in cards]
+def get_rank(current_cards):
+    current_cards.sort()
+    values = [cards[card] for card in current_cards]
     if len(set(values)) == 1:
         return 3
-    straight_value = check_straight(*cards)
+    straight_value = check_straight(*current_cards)
     if straight_value:
         if straight_value == 14:
             return 5
--- test_poker.py
+++ test_poker.py
@@ -4,22 +4,15 @@ from poker import check_straight, check_3ofa_kind, check_royal_flush, play_cards
 
 class TestPoker(unittest.TestCase):
 
-    def check_straight(card1, card2, card3):
-        cards = [card1, card2, card3]
-        values = []
-        for card in cards:
-            if card[1:] == 'A':
-                values.append(1)
-                values.append(14)
-            else:
-                values.append(int(card[1:]))
+    def test_check_straight(self):
+        self.assertEqual(check_straight('S10', 'SJ', 'SQ'), 12)
+        self.assertEqual(check_straight('S9', 'S10', 'SJ'), 11)
+        self.assertEqual(check_straight('SA', 'SK', 'SQ'), 14)
 
-    def check_3ofa_kind(card1, card2, card3):
-        face_cards = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, '10': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4,
-                      '3': 3,
-                      '2': 2}
-        cards = [card1, card2, card3]
-        values = [face_cards.get(card[1:], int(card[1:])) for card in cards]
+    def test_check_3ofa_kind(self):
+        self.assertEqual(check_3ofa_kind('SA', 'SK', 'SQ'), 14)
+        self.assertEqual(check_3ofa_kind('S2', 'S2', 'S2'), 2)
+        self.assertEqual(check_3ofa_kind('S2', 'S3', 'S4'), 14)
 
     def test_check_royal_flush(self):
         self.assertEqual(check_royal_flush('SA', 'SK', 'SQ'), 14)
@@ -27,8 +20,9 @@ class TestPoker(unittest.TestCase):
         self.assertEqual(check_royal_flush('SA', 'SK', 'S2'), 0)
 
     def test_play_cards(self):
-        def test_play_cards(self):
-            self.assertEqual(play_cards('S2', 'S3', 'S4', 'S5', 'S6', 'S7'), 0)
+        self.assertEqual(play_cards('S2', 'S3', 'S4', 'S5', 'S6', 'S7'), 0)
+        self.assertEqual(play_cards('SA', 'SK', 'SQ', 'S2', 'S3', 'S4'), 1)
+        self.assertEqual(play_cards('S2', 'S3', 'S4', 'SA', 'SK', 'SQ'), -1)
 
 
 if __name__ == '__main__':
